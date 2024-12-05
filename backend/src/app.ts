1c1d43a539d1f653d689ed58b6617e2ae7a48052
BrianChiastellino <chiastellino.dev@gmail.com>
2024-12-04 23:13:34 -0300

    fix url

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,24 +1,23 @@
 import cors from 'cors';
 import morgan from 'morgan';
-import express, { Response } from 'express';
+import express from 'express';
 
 import playerRouter from './routes/player-routes';
 import userRouter from './routes/user-routes';
 import authRouter from './routes/auth-routes'
-import { authRole } from './middlewares/auth-role';
 import { authenticateJWT } from './middlewares/auth-jwt';
-import { UserRole } from './enums/user-role.enum';
 
 const app = express();
-const url = process.env.APP_BASE_URL;
+const BASE_URL = process.env.APP_BASE_URL;
 
 app.use( cors());
 app.use( morgan('dev'));
 app.use( express.json());
 
 // implementar rutas, controladores, y modelos
-app.use(`${ url }/auth` ,authRouter );
-app.use(`${ url }/users`, authenticateJWT, userRouter );
+
+app.use(`${ BASE_URL }/auth` ,authRouter );
+app.use(`${ BASE_URL }/users`, userRouter );
 // app.use( `${ url }/players`, authenticateJWT, playerRouter );
 
 
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,12 +5,13 @@ import { AppDataSource } from './database/data-source';
 async function main() {
     try {
         const PORT = process.env.APP_PORT ?? 8081;
+        const BASE_URL = process.env.APP_BASE_URL ?? 'api/v1';
 
         await AppDataSource.initialize();
-        console.info(`Database is running on port ${ process.env.DB_PORT }`);
+        console.info(`Database is running on port: ${ process.env.DB_PORT }`);
 
         app.listen( PORT, () => {
-            console.log(`App is running on http://localhost:/${PORT} `);
+            console.log(`App is running on http://localhost:${ PORT + BASE_URL } `);
         });
 
     } catch (error) {
