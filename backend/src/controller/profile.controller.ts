1d099e359243d3058825ac09d967c3dadb8d58db
Brian Chaste <b.chiastellino@gmail.com>
2024-12-10 14:16:14 -0300

    profile service

--- backend/src/controller/profile.controller.ts
+++ backend/src/controller/profile.controller.ts
@@ -1,19 +1,19 @@
 import { Request, Response } from "express";
 import { JwtPayloadDTO } from "../dto/jwt-paylaod.dto";
 import { User } from "../models/user.model";
+import profileService from "../services/profile.service";
 
 class ProfileController {
 
     async get  ( req : Request, res : Response ) {
         try{
-
             const id = req.payload?.subjectId;
-            const user = await User.findOneBy({ id });
+            const user = await profileService.get( id! );
     
             if ( !user || !id )
                 throw new Error(`Usuario con id ${ id } no encontrado`);
-    
-            res.status(200).json( user );
+
+            res.status(200).json({ msg : 'Get', user });
         } catch ( error ) {
             if ( error instanceof Error ){
                 res.status(400).json({ msg : `${ error.message }`});
@@ -22,13 +22,38 @@ class ProfileController {
     };
 
     update ( req : Request, res : Response ) {
-        const { payload } = req;
-        res.status(200).json({ payload });
+        try {
+            const id = req.payload?.subjectId;
+            const updateResult = profileService.update(id! , req.body);
+
+            if ( !id || !updateResult ) 
+                return new Error(`Usuario con id ${ id } no existe`);
+
+            res.status(200).json({ msg : 'Update', updateResult });
+
+        } catch ( error ) {
+            if ( error instanceof Error ) {
+                res.status(400).json({ msg : `${ error.message }`});
+            };
+        }
+
     };
 
     delete ( req : Request, res : Response ) {
-        const { payload } = req;
-        res.status(200).json({ payload });
+        try {
+            const id = req.payload?.subjectId;
+            const deleteResult = profileService.delete( id! );
+    
+            if ( !id || !deleteResult )
+                return new Error(`Usuario con id ${ id } no existe`);
+    
+            res.status(200).json({ msg : 'Delete', deleteResult });
+            
+        } catch ( error ) {
+            if ( error instanceof Error ) {
+                res.status(400).json({ msg : `${ error.message }`});
+            };
+        }
     };
 
 }
---
+++ backend/src/services/profile.service.ts
@@ -0,0 +1,21 @@
+import { User } from "../models/user.model";
+import { DeleteResult, UpdateResult } from "typeorm";
+
+
+
+class ProfileService {
+
+    async get ( id : string ) : Promise<User | null> {
+        return await User.findOneBy({ id });
+    };
+
+    async update ( id : string, user : User ) : Promise<UpdateResult> {
+        return await User.update({ id }, user )
+    };
+
+    async delete ( id : string ) : Promise< DeleteResult> {
+        return await User.delete({ id })
+    };
+}
+
+export default new ProfileService();
